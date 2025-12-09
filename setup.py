d2d87836090a0e1d88f29cad38b3d3f6549c6809
1Sujal <103548879+1Sujal@users.noreply.github.com>
2025-12-09 10:16:49 +0545

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="github-folder-cloner",
+    version="0.1.0",
+    description="Clone a specific folder from a GitHub repo without cloning the entire repo",
+    author="Sujal Bhattarai",
+    author_email="your-email@example.com",
+    url="https://github.com/1Sujal/github-folder-cloner",
+    packages=find_packages(),
+    py_modules=["main"],
+    install_requires=[
+        "requests",
+    ],
+    entry_points={
+        "console_scripts": [
+            "github-folder-cloner=main:main",
+        ],
+    },
+    python_requires='>=3.8',
+)
